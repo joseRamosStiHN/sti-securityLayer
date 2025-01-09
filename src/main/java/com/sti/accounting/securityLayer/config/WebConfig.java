10231983718fe9a2dea0cd6a6df064f78e8285ea
Lcaceres97 <lcaceres@stiglobals.com>
2025-01-08 22:06:28 -0600

    cambios modulo de seguridad

---
+++ src/main/java/com/sti/accounting/securityLayer/config/WebConfig.java
@@ -0,0 +1,14 @@
+package com.sti.accounting.securityLayer.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**").allowedHeaders("*").allowedMethods("*").allowedOrigins("*");
+    }
+}
