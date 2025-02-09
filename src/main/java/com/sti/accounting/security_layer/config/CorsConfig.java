e0d1b244afb5931daea2817b3a87c98e1a8e2bef
Jose Manuel Ramos Morel <jramos@stiglobals.com>
2025-02-09 13:42:56 -0600

    CORS fix

--- src/main/java/com/sti/accounting/security_layer/config/CorsConfig.java
+++ src/main/java/com/sti/accounting/security_layer/config/CorsConfig.java
@@ -6,22 +6,22 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
-@Configuration
+//@Configuration
 public class CorsConfig {
 
-    @Bean
-    public CorsFilter corsFilter() {
-        CorsConfiguration config = new CorsConfiguration();
-        config.setAllowCredentials(true);
-        config.addAllowedOriginPattern("*"); // Aceptar todos los orígenes (ajustar para producción)
-        config.addAllowedHeader("*"); // Permite cualquier encabezado
-        config.addAllowedMethod("*"); // Permite todos los métodos HTTP
-
-
-        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        source.registerCorsConfiguration("/**", config);
-        return new CorsFilter(source);
-    }
+//    @Bean
+//    public CorsFilter corsFilter() {
+//        CorsConfiguration config = new CorsConfiguration();
+//        config.setAllowCredentials(true);
+//        config.addAllowedOriginPattern("*"); // Aceptar todos los orígenes (ajustar para producción)
+//        config.addAllowedHeader("*"); // Permite cualquier encabezado
+//        config.addAllowedMethod("*"); // Permite todos los métodos HTTP
+//
+//
+//        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+//        source.registerCorsConfiguration("/**", config);
+//        return new CorsFilter(source);
+//    }
 
 
 }
--- src/main/java/com/sti/accounting/security_layer/config/WebConfig.java
+++ src/main/java/com/sti/accounting/security_layer/config/WebConfig.java
@@ -1,15 +1,45 @@
 package com.sti.accounting.security_layer.config;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-//@Configuration
-public class WebConfig implements WebMvcConfigurer {
+import java.util.List;
 
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**").allowedHeaders("*").allowedMethods("*").allowedOrigins("*");
+@Configuration
+@EnableWebSecurity
+public class WebConfig  {
+
+    @Value("${app.allow.origins}")
+    private String allowedOrigins;
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http
+                .csrf(AbstractHttpConfigurer::disable)
+                .cors(cors -> cors.configurationSource(corsConfigurationSource()))
+                .build();
     }
 
+    @Bean
+    public UrlBasedCorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowCredentials(true);
+        config.setAllowedOrigins(List.of(allowedOrigins.split(","))); // Ajustar para producción DDoS
+        config.addAllowedHeader("*");
+        config.addAllowedMethod("*");
 
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", config);
+        return source;
+    }
 }
